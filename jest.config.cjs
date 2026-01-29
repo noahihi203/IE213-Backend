43dbacca466ca7c8729e5b7c094a4652412a418b
noahihi203 <da02470247@gmail.com>
2026-01-29 21:16:14 +0700

    chore: add jest configuration for TypeScript testing

---
+++ jest.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.test.ts"],
+  coverageDirectory: "coverage",
+  verbose: true,
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+        isolatedModules: true,
+        diagnostics: {
+          ignoreCodes: [6133, 151002],
+        },
+      },
+    ],
+  },
+};
