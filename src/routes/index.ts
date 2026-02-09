bbd2a91c4939325d7bfe7004924816131b4edbc8
noahihi203 <da02470247@gmail.com>
2026-02-09 14:21:13 +0700

    feat: Refactor route imports and organization in index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import accessRouter from "./access/index.js";
-import userRouter from "./user/index.js"
+import userRouter from "./user/index.js";
+import categoryRouter from "./category/index.js";
 const router = express.Router();
 // import { apiKey, permission } from "../auth/checkAuth";
 // import { pushToLogDiscord } from "../middlewares/index";
+router.use("/v1/api/category", categoryRouter);
 router.use("/v1/api", accessRouter);
-router.use("/v1/api", userRouter);
+router.use("/v1/api/user", userRouter);
 export default router;
