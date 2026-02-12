926836d18ec30c9439f98e62729262a6f5a4c2e5
noahihi203 <da02470247@gmail.com>
2026-02-12 15:37:29 +0700

    feat: Add like, unlike, and share functionalities to PostController

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 import { SuccessResponse } from "../core/success.response.js";
 import PostService from "../services/post.service.js";
 import { BadRequestError } from "../core/error.response.js";
+import LikeService from "../services/like.service.js";
+import { convertToObjectIdMongodb } from "../utils/index.js";
+import { Schema } from "mongoose";
+import ShareService from "../services/share.service.js";
 
 class PostController {
   getAllPosts = async (req: Request, res: Response) => {
@@ -64,6 +68,74 @@ class PostController {
     new SuccessResponse({
       message: "Delete post success!",
       metadata: await PostService.deletePost(postId),
+    }).send(res);
+  };
+
+  likePost = async (req: Request, res: Response) => {
+    const postId = req.params.postId;
+    if (Array.isArray(postId))
+      throw new BadRequestError("Invalid postId format!");
+
+    const userId = req.user?.userId;
+    if (!userId) throw new BadRequestError("Invalid userId");
+
+    const likeParams = {
+      targetId: convertToObjectIdMongodb(postId),
+      userId: convertToObjectIdMongodb(userId),
+    };
+
+    console.log("likeParams", likeParams);
+
+    new SuccessResponse({
+      message: "Like post success!",
+      metadata: await LikeService.likePost(likeParams),
+    }).send(res);
+  };
+
+  unlikePost = async (req: Request, res: Response) => {
+    const postId = req.params.postId;
+    if (Array.isArray(postId))
+      throw new BadRequestError("Invalid postId format!");
+
+    const userId = req.user?.userId;
+    if (!userId) throw new BadRequestError("Invalid userId");
+
+    const likeParams = {
+      targetId: convertToObjectIdMongodb(postId),
+      userId: convertToObjectIdMongodb(userId),
+    };
+
+    new SuccessResponse({
+      message: "Unlike post success!",
+      metadata: await LikeService.unLikePost(likeParams),
+    }).send(res);
+  };
+
+  sharePost = async (req: Request, res: Response) => {
+    const postId = req.params.postId;
+    if (Array.isArray(postId))
+      throw new BadRequestError("Invalid postId format!");
+
+    const userId = req.user?.userId;
+    if (!userId) throw new BadRequestError("Invalid userId");
+
+    const shareConent = {
+      postId: convertToObjectIdMongodb(postId),
+      userId: convertToObjectIdMongodb(userId),
+      platform: req.body.platform as string,
+      message: req.body.message as string,
+    };
+
+    new SuccessResponse({
+      message: "Share post success!",
+      metadata: await ShareService.createShare(shareConent),
+    }).send(res);
+  };
+
+  getTrendingPosts = async (req: Request, res: Response) => {
+    new SuccessResponse({
+      message: "Get trending post success!",
+      metadata: await PostService.getTrendingPosts(),
     }).send(res);
   };
 }
