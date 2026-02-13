d8bf79e19f3577a99724a2bb8669273fc256e317
noahihi203 <da02470247@gmail.com>
2026-02-14 00:58:14 +0700

    feat: Enhance NotificationController with error handling and CRUD operations

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -1,5 +1,73 @@
+import { Request, Response } from "express";
+import { SuccessResponse } from "../core/success.response.js";
+import NotificationService from "../services/notification.service.js";
+import { convertToObjectIdMongodb } from "../utils/index.js";
+import { BadRequestError } from "../core/error.response.js";
+
 class NotificationController {
-    
+  //    - [ ] getUserNotifications (GET /v1/api/notifications)
+  getUserNotifications = async (req: Request, res: Response) => {
+    if (typeof req.user?.userId !== "string")
+      throw new BadRequestError("Invalid userId format!");
+    new SuccessResponse({
+      message: "Get user notification success!",
+      metadata: await NotificationService.getUserNotifications({
+        userId: convertToObjectIdMongodb(req.user?.userId),
+        filter: req.body,
+      }),
+    }).send(res);
+  };
+  //   - [ ] markAsRead (PUT /v1/api/notifications/:notificationId/read)
+  markAsRead = async (req: Request, res: Response) => {
+    if (typeof req.user?.userId !== "string")
+      throw new BadRequestError("Invalid userId format!");
+
+    if (typeof req.params.notificationId !== "string")
+      throw new BadRequestError("Invalid userId format!");
+
+    new SuccessResponse({
+      message: "Mark as read notification success!",
+      metadata: await NotificationService.markAsRead({
+        userId: convertToObjectIdMongodb(req.user?.userId),
+        notiId: convertToObjectIdMongodb(req.params.notificationId),
+      }),
+    }).send(res);
+  };
+
+  //   - [ ] markAllAsRead (PUT /v1/api/notifications/read-all)
+  markAllAsRead = async (req: Request, res: Response) => {
+    if (typeof req.user?.userId !== "string")
+      throw new BadRequestError("Invalid userId format!");
+    new SuccessResponse({
+      message: "Mark all as read notification success!",
+      metadata: await NotificationService.markAllAsRead(
+        convertToObjectIdMongodb(req.user?.userId),
+      ),
+    }).send(res);
+  };
+
+  //   - [ ] deleteNotification (DELETE /v1/api/notifications/:notificationId)
+  deleteNotification = async (req: Request, res: Response) => {
+    if (typeof req.params.notificationId !== "string")
+      throw new BadRequestError("Invalid userId format!");
+    new SuccessResponse({
+      message: "Delete notification success!",
+      metadata: await NotificationService.deleteNotification(
+        convertToObjectIdMongodb(req.params.notificationId),
+      ),
+    }).send(res);
+  };
+  //   - [ ] deleteAllRead (DELETE /v1/api/notifications/read)
+  deleteAllRead = async (req: Request, res: Response) => {
+    if (typeof req.user?.userId !== "string")
+      throw new BadRequestError("Invalid userId format!");
+    new SuccessResponse({
+      message: "Delete all notification success!",
+      metadata: await NotificationService.deleteAllRead(
+        convertToObjectIdMongodb(req.user?.userId),
+      ),
+    }).send(res);
+  };
 }
 
 export default new NotificationController();
