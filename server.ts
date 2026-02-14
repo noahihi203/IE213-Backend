2b809509a73ca6f1623793080be3371e13ed8713
noahihi203 <da02470247@gmail.com>
2026-02-14 21:01:03 +0700

    feat: Initialize Kafka producer and consumer in server bootstrap process

--- server.ts
+++ server.ts
@@ -2,7 +2,30 @@ import app from "./src/app";
 // const { initRedis } = require("./src/services/redis.service");
 
 const PORT = process.env.PORT || 5000;
+// server.ts
+import { KafkaConsumer } from "./src/services/kafka/kafka.cosumer.js";
+import { kafkaProducer } from "./src/services/kafka/kafka.producer.js";
 
+async function bootstrap() {
+  try {
+    // 1. Kết nối Producer trước (để API có thể gửi tin)
+    await kafkaProducer.connect();
+
+    // 2. Kết nối Consumer để bắt đầu xử lý job ngầm
+    const consumer = new KafkaConsumer();
+    await consumer.connect();
+
+    console.log(" Notification System is ready with Kafka");
+
+    // 3. Khởi động Express Server (hoặc framework bạn dùng)
+    // app.listen(3000, () => ...)
+  } catch (error) {
+    console.error("❌ Failed to start application:", error);
+    process.exit(1); // Thoát nếu không kết nối được hạ tầng quan trọng
+  }
+}
+
+bootstrap();
 const server = app.listen(PORT, async () => {
   // await initRedis();
   console.log(`WSV eCommerce start with port ${PORT}`);
