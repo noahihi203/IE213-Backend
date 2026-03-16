5f5b19ab3931c2c4e208521b173a6d79ce7b20fc
noahihi203 <da02470247@gmail.com>
2026-03-16 14:17:22 +0700

    fix: Update success messages and metadata retrieval in StatisticController methods

--- src/controllers/statistics.controller.ts
+++ src/controllers/statistics.controller.ts
@@ -11,27 +11,27 @@ class StatisticController {
   };
   getUserStats = async (req: Request, res: Response) => {
     new SuccessResponse({
-      message: "Get dashboard stats success",
-      metadata: await StatisTicService.getDashboardStats(),
+      message: "Get user statistics success",
+      metadata: await StatisTicService.getUserStatistics(),
     }).send(res);
   };
   getPostStats = async (req: Request, res: Response) => {
     new SuccessResponse({
-      message: "Get dashboard stats success",
-      metadata: await StatisTicService.getDashboardStats(),
+      message: "Get post statistics success",
+      metadata: await StatisTicService.getPostStatistics(),
     }).send(res);
   };
   getActivityStats = async (req: Request, res: Response) => {
     new SuccessResponse({
-      message: "Get dashboard stats success",
-      metadata: await StatisTicService.getDashboardStats(),
+      message: "Get activity statistics stats success",
+      metadata: await StatisTicService.getActivityStatistics(),
     }).send(res);
   };
-    getCategoryStats = async (req: Request, res: Response) => {
-      new SuccessResponse({
-        message: "Get dashboard stats success",
-        metadata: await StatisTicService.getDashboardStats(),
-      }).send(res);
+  getCategoryStats = async (req: Request, res: Response) => {
+    new SuccessResponse({
+      message: "Get category statistics success",
+      metadata: await StatisTicService.getCategoryStatistics(),
+    }).send(res);
   };
 }
 
